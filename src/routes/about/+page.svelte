cb160142ab5eed5aa8be534ac194e043786d82c0
thomas <t.moerch@gmail.com>
2024-09-13 22:34:45 +0200

    Added more revelant About text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,23 +1,21 @@
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+	<title>Ask Odin?</title>
+	<meta name="description" content="About" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>What is?</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		This app lets you get a list of channels you follow that are live, filtered by a set of parameters.
 	</p>
-
-	<pre>npm create svelte@latest</pre>
 
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		I made it because I moderate a channel and wanted a tool to find potential raid targets for the streamer. I follow a <em>lot</em> of channels and needed a more condensed and structured overview. I also want to avoid raiding into streams as they end.
 	</p>
 
+	<p>
+		usage instructions to come here once the app is more complete
+	</p>
 
 </div>
